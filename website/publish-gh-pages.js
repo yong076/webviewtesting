79ed8aec447c30c916eea8c05ea2bf2b8d4918b2
Konstantin Raev <bestander@fb.com>
2016-02-08 10:14:27 +0000

    switching to circle.ci for website deployments

--- website/publish-gh-pages.js
+++ website/publish-gh-pages.js
@@ -6,66 +6,81 @@
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
  */
-'use strict';
+`use strict`;
 
-const branch = process.env.TRAVIS_BRANCH;
-const isPullRequest = process.env.TRAVIS_PULL_REQUEST !== 'false';
-const isTravis = process.env.TRAVIS;
-const remoteBranch = 'git://github.com/facebook/react-native.git';
-require('shelljs/global');
+const {
+  CIRCLE_BRANCH,
+  CIRCLE_PROJECT_USERNAME,
+  CIRCLE_PROJECT_REPONAME,
+  CIRCLE_COMPARE_URL,
+  CI_PULL_REQUESTS,
+  CI_PULL_REQUEST,
+  CIRCLE_USERNAME,
+} = process.env;
 
-if (!which('git')) {
-  echo('Sorry, this script requires git');
+// TODO temp
+const remoteBranch = 'git://github.com/bestander/react-native.git';
+require(`shelljs/global`);
+
+if (!which(`git`)) {
+  echo(`Sorry, this script requires git`);
   exit(1);
 }
 
 let version;
-if (branch.indexOf('-stable') !== -1) {
-  version = branch.slice(0, branch.indexOf('-stable'));
+if (branch.indexOf(`-stable`) !== -1) {
+  version = CIRCLE_BRANCH.slice(0, branch.indexOf(`-stable`));
 }
 
-console.log({isPullRequest, isTravis, version})
-exit(0);
-
+console.log({
+              CIRCLE_BRANCH,
+              CIRCLE_PROJECT_USERNAME,
+              CIRCLE_PROJECT_REPONAME,
+              CIRCLE_COMPARE_URL,
+              CI_PULL_REQUESTS,
+              CI_PULL_REQUEST,
+              CIRCLE_USERNAME,
+              version
+            });
 
 if (exec(`node ./server/generate.js`).code !== 0) {
-  echo('Error: Generating HTML failed');
+  echo(`Error: Generating HTML failed`);
 }
 
 if (!isPullRequest && isTravis && version) {
   echo(`Building stable branch ${version}, preparing to push to gh-pages`);
   // if code is running in a branch in CI, commit changes to gh-pages branch
-  cd('build');
-  rm('-rf', 'react-native-gh-pages');
+  cd(`build`);
+  rm(`-rf`, `react-native-gh-pages`);
 
   if (exec(`git clone ${remoteBranch} react-native-gh-pages`).code !== 0) {
-    echo('Error: Git clone failed');
+    echo(`Error: Git clone failed`);
     exit(1);
   }
 
-  cd('react-native-gh-pages');
+  cd(`react-native-gh-pages`);
 
   const checkoutCode = exec(`git checkout origin/gh-pages`).code;
   checkoutCode += exec(`git checkout -b gh-pages`).code;
   checkoutCode += exec(`git branch --set-upstream-to=origin/gh-pages`).code;
 
   if (checkoutCode !== 0) {
-    echo('Error: Git checkout gh-pages failed');
+    echo(`Error: Git checkout gh-pages failed`);
     exit(1);
   }
 
-  rm('-rf', `releases/${version}`);
-  mkdir('-p', `releases/${version}`);
-  exec(`cp -R ../react-native/* releases/${version}`);
+  rm(`-rf`, `releases/${version}`);
+  mkdir(`-p`, `releases/${version}`);
+  exec('cp -R ../react-native/* releases/${version}');
   exec(`cp ../../../circle.yml .`);
 
   exec(`git status`);
   exec(`git add -A .`);
   if (exec(`git diff-index --quiet HEAD --`).code !== 0) {
     const checkInCode = exec(`git commit -m "update website"`).code;
-    checkInCode += exec(`git push origin gh-pages`).code;
+//    checkInCode += exec(`git push origin gh-pages`).code;
     if (checkInCode !== 0) {
-      echo('Error: Git checkout gh-pages failed');
+      echo(`Error: Git checkout gh-pages failed`);
       exit(1);    
     }
   }
