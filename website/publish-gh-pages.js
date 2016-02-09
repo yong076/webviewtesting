4d3693e155ed22aa169fb30b72950ba0c2206ad8
Konstantin Raev <bestander@fb.com>
2016-02-09 14:18:20 +0000

    ci improvement

--- website/publish-gh-pages.js
+++ website/publish-gh-pages.js
@@ -8,12 +8,12 @@
  */
 'use strict';
 
-const CIRCLE_BRANCH = process.env.CIRCLE_BRANCH;
-const CIRCLE_PROJECT_USERNAME = process.env.CIRCLE_PROJECT_USERNAME;
+const CIRCLE_BRANCH = process.env.CIRCLE_BRANCH || 'master';
+const CIRCLE_PROJECT_USERNAME = process.env.CIRCLE_PROJECT_USERNAME || 'bestander';
 const CIRCLE_PROJECT_REPONAME = process.env.CIRCLE_PROJECT_REPONAME;
 const CI_PULL_REQUESTS = process.env.CI_PULL_REQUESTS;
 const CI_PULL_REQUEST = process.env.CI_PULL_REQUEST;
-const GIT_USER = process.env.GIT_USER;
+const GIT_USER = process.env.GIT_USER || 'bestander';
 // TODO hardcode
 const remoteBranch = `https://${GIT_USER}@github.com/bestander/react-native.git`;
 require(`shelljs/global`);
@@ -41,14 +41,11 @@ console.log({
 });
 
 rm(`-rf`, `build`);
-if (exec(`RN_DEPLOYMENT_PATH=releases/${version} node ./server/generate.js`).code !== 0) {
-  echo(`Error: Generating HTML failed`);
-  exit(1);
-}
+mkdir(`-p`, `build`);
 
 // TODO hardcode
-if (!!version && !CI_PULL_REQUEST && CIRCLE_PROJECT_USERNAME === `bestander`) {
-  echo(`Building stable branch ${version}, preparing to push to gh-pages`);
+if (!CI_PULL_REQUEST && CIRCLE_PROJECT_USERNAME === `bestander`) {
+  echo(`Building branch ${version}, preparing to push to gh-pages`);
   // if code is running in a branch in CI, commit changes to gh-pages branch
   cd(`build`);
   rm(`-rf`, `react-native-gh-pages`);
@@ -67,22 +64,42 @@ if (!!version && !CI_PULL_REQUEST && CIRCLE_PROJECT_USERNAME === `bestander`) {
     echo(`Error: Git checkout gh-pages failed`);
     exit(1);
   }
+  // generate to releases/XX when branch name indicates that it is some sort of release
+  if (!!version) {
+    rm(`-rf`, `releases/${version}`);
+    mkdir(`-p`, `releases/${version}`);
 
-  rm(`-rf`, `releases/${version}`);
-  mkdir(`-p`, `releases/${version}`);
-  exec(`cp -R ../react-native/* releases/${version}`);
-  exec(`cp ../../../circle.yml .`);
-
-  exec(`git status`);
-  exec(`git add -A .`);
-  if (exec(`git diff-index --quiet HEAD --`).code !== 0) {
-    if (exec(`git commit -m "Updated docs for ${version}"`).code !== 0) {
-      echo(`Error: Git commit gh-pages failed`);
-      exit(1);    
-    }
-    if (exec(`git push origin gh-pages`).code !== 0) {
-      echo(`Error: Git push gh-pages failed`);
+    if (exec(`RN_DEPLOYMENT_PATH=releases/${version} node ../../server/generate.js`).code !== 0) {
+      echo(`Error: Generating HTML failed`);
       exit(1);
     }
+    exec(`cp -R ../../react-native/* releases/${version}`);
   }
+  // if current commit is tagged "latest" we do a release to gh-pages root
+  let currentCommit = exec(`git rev-parese HEAD`);
+  let latestTagCommit = exec(`git rev-list -n latest`);
+  if (currentCommit === latestTagCommit) {
+    // leave only releases folder
+    rm(`-rf`, ls(`*`).filter(name => name !== 'releases'));
+    if (exec(`node ../../server/generate.js`).code !== 0) {
+      echo(`Error: Generating HTML failed`);
+      exit(1);
+    }
+    exec(`cp -R ../../react-native/* .`);
+  }
+  if (false && currentCommit === latestTagCommit || version) {
+    exec(`git status`);
+    exec(`git add -A .`);
+    if (exec(`git diff-index --quiet HEAD --`).code !== 0) {
+      if (exec(`git commit -m "Updated docs for ${version}"`).code !== 0) {
+        echo(`Error: Git commit gh-pages failed`);
+        exit(1);
+      }
+      if (exec(`git push origin gh-pages`).code !== 0) {
+        echo(`Error: Git push gh-pages failed`);
+        exit(1);
+      }
+    }
+  }
+
 }
